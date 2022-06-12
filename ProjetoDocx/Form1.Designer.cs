1252e714a5393843144dd9cc63237a3eb07db112
josec <josec@DESKTOP-3UVR740>
2022-06-12 19:20:01 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoDocx/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace ProjetoDocx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnMontar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbReclamante = new System.Windows.Forms.TextBox();
+            this.tbReclamada = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbLocalVistoria = new System.Windows.Forms.TextBox();
+            this.tbEndLocal = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbCidadeEmissao = new System.Windows.Forms.TextBox();
+            this.tbDataVistoria = new System.Windows.Forms.MaskedTextBox();
+            this.tbHoraInicio = new System.Windows.Forms.MaskedTextBox();
+            this.tbDataEmissao = new System.Windows.Forms.MaskedTextBox();
+            this.tbProcesso = new System.Windows.Forms.MaskedTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tbDataIniPeriodo = new System.Windows.Forms.MaskedTextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.tbDataFimPeriodo = new System.Windows.Forms.MaskedTextBox();
+            this.Funcao = new System.Windows.Forms.Label();
+            this.tbFuncaoExercida = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbTesReclamante = new System.Windows.Forms.TextBox();
+            this.btnInsLwReclamante = new System.Windows.Forms.Button();
+            this.btnInsLwReclamada = new System.Windows.Forms.Button();
+            this.tbTesReclamada = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lboxReclamante = new System.Windows.Forms.ListBox();
+            this.lboxReclamada = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMontar
+            // 
+            this.btnMontar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            resources.ApplyResources(this.btnMontar, "btnMontar");
+            this.btnMontar.Name = "btnMontar";
+            this.btnMontar.UseVisualStyleBackColor = false;
+            this.btnMontar.Click += new System.EventHandler(this.BtnMontar_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // tbReclamante
+            // 
+            resources.ApplyResources(this.tbReclamante, "tbReclamante");
+            this.tableLayoutPanel1.SetColumnSpan(this.tbReclamante, 3);
+            this.tbReclamante.Name = "tbReclamante";
+            // 
+            // tbReclamada
+            // 
+            resources.ApplyResources(this.tbReclamada, "tbReclamada");
+            this.tableLayoutPanel1.SetColumnSpan(this.tbReclamada, 3);
+            this.tbReclamada.Name = "tbReclamada";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // tbLocalVistoria
+            // 
+            resources.ApplyResources(this.tbLocalVistoria, "tbLocalVistoria");
+            this.tableLayoutPanel1.SetColumnSpan(this.tbLocalVistoria, 3);
+            this.tbLocalVistoria.Name = "tbLocalVistoria";
+            // 
+            // tbEndLocal
+            // 
+            resources.ApplyResources(this.tbEndLocal, "tbEndLocal");
+            this.tableLayoutPanel1.SetColumnSpan(this.tbEndLocal, 3);
+            this.tbEndLocal.Name = "tbEndLocal";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // tbCidadeEmissao
+            // 
+            resources.ApplyResources(this.tbCidadeEmissao, "tbCidadeEmissao");
+            this.tableLayoutPanel1.SetColumnSpan(this.tbCidadeEmissao, 3);
+            this.tbCidadeEmissao.Name = "tbCidadeEmissao";
+            // 
+            // tbDataVistoria
+            // 
+            resources.ApplyResources(this.tbDataVistoria, "tbDataVistoria");
+            this.tbDataVistoria.Name = "tbDataVistoria";
+            this.tbDataVistoria.ValidatingType = typeof(System.DateTime);
+            // 
+            // tbHoraInicio
+            // 
+            resources.ApplyResources(this.tbHoraInicio, "tbHoraInicio");
+            this.tbHoraInicio.Name = "tbHoraInicio";
+            this.tbHoraInicio.ValidatingType = typeof(System.DateTime);
+            // 
+            // tbDataEmissao
+            // 
+            resources.ApplyResources(this.tbDataEmissao, "tbDataEmissao");
+            this.tbDataEmissao.Name = "tbDataEmissao";
+            this.tbDataEmissao.ValidatingType = typeof(System.DateTime);
+            // 
+            // tbProcesso
+            // 
+            resources.ApplyResources(this.tbProcesso, "tbProcesso");
+            this.tableLayoutPanel1.SetColumnSpan(this.tbProcesso, 3);
+            this.tbProcesso.Name = "tbProcesso";
+            this.tbProcesso.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals;
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.label13, 2, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tbProcesso, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tbReclamante, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tbReclamada, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tbDataVistoria, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tbHoraInicio, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tbLocalVistoria, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.tbEndLocal, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tbDataIniPeriodo, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label12, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 0, 11);
+            this.tableLayoutPanel1.Controls.Add(this.tbDataEmissao, 1, 11);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 0, 10);
+            this.tableLayoutPanel1.Controls.Add(this.tbCidadeEmissao, 1, 10);
+            this.tableLayoutPanel1.Controls.Add(this.tbDataFimPeriodo, 3, 7);
+            this.tableLayoutPanel1.Controls.Add(this.Funcao, 0, 8);
+            this.tableLayoutPanel1.Controls.Add(this.tbFuncaoExercida, 1, 8);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // tbDataIniPeriodo
+            // 
+            resources.ApplyResources(this.tbDataIniPeriodo, "tbDataIniPeriodo");
+            this.tbDataIniPeriodo.Name = "tbDataIniPeriodo";
+            this.tbDataIniPeriodo.ValidatingType = typeof(System.DateTime);
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // tbDataFimPeriodo
+            // 
+            resources.ApplyResources(this.tbDataFimPeriodo, "tbDataFimPeriodo");
+            this.tbDataFimPeriodo.Name = "tbDataFimPeriodo";
+            this.tbDataFimPeriodo.ValidatingType = typeof(System.DateTime);
+            // 
+            // Funcao
+            // 
+            resources.ApplyResources(this.Funcao, "Funcao");
+            this.Funcao.Name = "Funcao";
+            // 
+            // tbFuncaoExercida
+            // 
+            resources.ApplyResources(this.tbFuncaoExercida, "tbFuncaoExercida");
+            this.tableLayoutPanel1.SetColumnSpan(this.tbFuncaoExercida, 3);
+            this.tbFuncaoExercida.Name = "tbFuncaoExercida";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // tbTesReclamante
+            // 
+            resources.ApplyResources(this.tbTesReclamante, "tbTesReclamante");
+            this.tbTesReclamante.Name = "tbTesReclamante";
+            this.tbTesReclamante.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDownHandler);
+            // 
+            // btnInsLwReclamante
+            // 
+            resources.ApplyResources(this.btnInsLwReclamante, "btnInsLwReclamante");
+            this.btnInsLwReclamante.Name = "btnInsLwReclamante";
+            this.btnInsLwReclamante.UseVisualStyleBackColor = true;
+            this.btnInsLwReclamante.Click += new System.EventHandler(this.btnInsLwReclamante_Click);
+            // 
+            // btnInsLwReclamada
+            // 
+            resources.ApplyResources(this.btnInsLwReclamada, "btnInsLwReclamada");
+            this.btnInsLwReclamada.Name = "btnInsLwReclamada";
+            this.btnInsLwReclamada.UseVisualStyleBackColor = true;
+            this.btnInsLwReclamada.Click += new System.EventHandler(this.btnInsLwReclamada_Click);
+            // 
+            // tbTesReclamada
+            // 
+            resources.ApplyResources(this.tbTesReclamada, "tbTesReclamada");
+            this.tbTesReclamada.Name = "tbTesReclamada";
+            this.tbTesReclamada.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDownHandler);
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // lboxReclamante
+            // 
+            this.lboxReclamante.FormattingEnabled = true;
+            resources.ApplyResources(this.lboxReclamante, "lboxReclamante");
+            this.lboxReclamante.Name = "lboxReclamante";
+            // 
+            // lboxReclamada
+            // 
+            this.lboxReclamada.FormattingEnabled = true;
+            resources.ApplyResources(this.lboxReclamada, "lboxReclamada");
+            this.lboxReclamada.Name = "lboxReclamada";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lboxReclamada);
+            this.Controls.Add(this.lboxReclamante);
+            this.Controls.Add(this.btnInsLwReclamada);
+            this.Controls.Add(this.tbTesReclamada);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnInsLwReclamante);
+            this.Controls.Add(this.tbTesReclamante);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnMontar);
+            this.Name = "Form1";
+            this.Activated += new System.EventHandler(this.Inicio);
+            this.Load += new System.EventHandler(this.Inicio);
+            this.Enter += new System.EventHandler(this.Inicio);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnMontar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbReclamante;
+        private System.Windows.Forms.TextBox tbReclamada;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbLocalVistoria;
+        private System.Windows.Forms.TextBox tbEndLocal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbCidadeEmissao;
+        private System.Windows.Forms.MaskedTextBox tbDataVistoria;
+        private System.Windows.Forms.MaskedTextBox tbHoraInicio;
+        private System.Windows.Forms.MaskedTextBox tbDataEmissao;
+        private System.Windows.Forms.MaskedTextBox tbProcesso;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbTesReclamante;
+        private System.Windows.Forms.Button btnInsLwReclamante;
+        private System.Windows.Forms.Button btnInsLwReclamada;
+        private System.Windows.Forms.TextBox tbTesReclamada;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ListBox lboxReclamante;
+        private System.Windows.Forms.ListBox lboxReclamada;
+        private System.Windows.Forms.MaskedTextBox tbDataFimPeriodo;
+        private System.Windows.Forms.MaskedTextBox tbDataIniPeriodo;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label Funcao;
+        private System.Windows.Forms.TextBox tbFuncaoExercida;
+    }
+}
+
---
+++ ProjetoDocx/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xceed.Words.NET;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ProjetoDocx
+{
+    public partial class Form1 : Form
+    {
+        List<string> listAcReclamante = new List<string>();
+        List<string> listAcReclamada = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnMontar_Click(object sender, EventArgs e)
+        {
+            string tx;
+            string path = Directory.GetCurrentDirectory();
+            try
+            {
+                using (DocX documento = DocX.Load(path + "\\modelo-v01.docx"))
+                {
+                    tx = tbProcesso.Text;
+                    documento.ReplaceText("#numProcesso", tx.Replace(',', '.'));
+
+                    tx = tbReclamante.Text;
+                    documento.ReplaceText("#nomeReclamante", tx.ToUpper());
+
+                    tx = tbReclamada.Text;
+                    documento.ReplaceText("#nomeReclamada", tx.ToUpper());
+
+                    documento.ReplaceText("#dataVistoria", tbDataVistoria.Text);
+                    documento.ReplaceText("#horaVistoria", tbHoraInicio.Text);
+                    documento.ReplaceText("#localVistoriado", tbLocalVistoria.Text);
+                    documento.ReplaceText("#enderecoVistoriado", tbEndLocal.Text);
+
+                    documento.ReplaceText("#inicioPeriodoReclamado", tbDataIniPeriodo.Text);
+                    documento.ReplaceText("#fimPeriodoReclamado", tbDataFimPeriodo.Text);
+
+                    tx = tbFuncaoExercida.Text;
+                    documento.ReplaceText("#FUNCAO", tx.ToUpper());
+
+                    // Montar local e data da emissão do laudo
+                    string[] meses = {"" , "janeiro", "fevereiro", "março", "abril", "maio", "junho",
+                                  "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
+
+                    string dma = tbDataEmissao.Text;
+                    if (!string.IsNullOrEmpty(dma) && !string.IsNullOrEmpty(tbCidadeEmissao.Text))
+                    {
+                        string dia, mes, ano, strmes, data;
+                        string[] arrDMA = dma.Split('/');
+                        dia = arrDMA[0];
+                        mes = arrDMA[1];
+                        strmes = meses[Int32.Parse(mes)];
+                        ano = arrDMA[2];
+                        data = $"{tbCidadeEmissao.Text}, {dia} de {strmes} de {ano}";
+
+                        documento.ReplaceText("#localDataEmissao", data);
+                    }
+
+                    documento.SaveAs(path + "\\novo-documento.docx");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+                return;
+            }
+
+            this.AbrirDoc("novo-documento.docx");
+
+            //Close this form.
+            //this.Close();
+        }
+
+        private void AbrirDoc(string nomeDoc)
+        {
+            object oMissing = System.Reflection.Missing.Value;
+
+            // string appPath = Path.GetDirectoryName(Application.ExecutablePath);
+            string path = Directory.GetCurrentDirectory();
+            string c = path + "\\novo-documento.docx";
+            object oTemplate = c;
+
+            Word._Application oWord;
+            Word._Document oDoc;
+            oWord = new Word.Application();
+
+            oDoc = oWord.Documents.Add(ref oTemplate, ref oMissing, ref oMissing, ref oMissing);
+
+            string nomes = "";
+            foreach (Word.Paragraph paragrafo in oDoc.Paragraphs)
+            {
+                string txtParagrafo = paragrafo.Range.Text;
+                if (txtParagrafo.IndexOf("#PeloReclamante") >= 0)
+                {
+                    nomes = "";
+                    foreach(string n in listAcReclamante)
+                    {
+                        nomes += "\t" + n + "\r\n";
+                    }
+                    paragrafo.Range.Select();
+                    paragrafo.Reset();
+                    paragrafo.set_Style("Normal");
+                    paragrafo.Range.Font.Size = 12;
+                    paragrafo.Range.Font.Name = "Arial";
+                    paragrafo.Range.Font.Bold = 0;
+                    paragrafo.Range.Text = nomes;
+                    
+                }
+                else if (txtParagrafo.IndexOf("#PelaReclamada") >= 0)
+                {
+                    nomes = "";
+                    foreach (string n in listAcReclamada)
+                    {
+                        nomes += "\t" + n + "\r\n";
+                    }
+                    paragrafo.Range.Select();
+                    paragrafo.Reset();
+                    paragrafo.set_Style("Normal");
+                    paragrafo.Range.Font.Size = 12;
+                    paragrafo.Range.Font.Name = "Arial";
+                    paragrafo.Range.Font.Bold = 0;
+                    paragrafo.Range.Text = nomes;
+                }
+            }
+            oWord.Visible = true;
+            try
+            {
+                //oDoc.Save();
+            }
+            catch (Exception e)
+            {
+                DialogResult dialogResult = MessageBox.Show(e.Message);
+                oDoc = null;
+            }
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                TextBox tb = (TextBox)sender;
+                string str = tb.Name;
+                if (str == "tbTesReclamante")
+                {
+                    this.InserirNaLista(tbTesReclamante, lboxReclamante, listAcReclamante);
+                }
+                else if (str == "tbTesReclamada")
+                {
+                    this.InserirNaLista(tbTesReclamada, lboxReclamada, listAcReclamada);
+                }
+            }
+        }
+
+        private void btnInsLwReclamante_Click(object sender, EventArgs e)
+        {
+            this.InserirNaLista(tbTesReclamante, lboxReclamante, listAcReclamante);
+        }
+
+        private void btnInsLwReclamada_Click(object sender, EventArgs e)
+        {
+            this.InserirNaLista(tbTesReclamada, lboxReclamada, listAcReclamada);
+        }
+
+        private bool InserirNaLista(TextBox tb, ListBox lb, List<string> lista)
+        {
+            if (!string.IsNullOrEmpty(tb.Text))
+            {
+                lista.Add(tb.Text);
+                tb.Text = null;
+                tb.Focus();
+
+                lb.DataSource = null;
+                lb.DataSource = lista;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void Inicio(object sender, EventArgs e)
+        {
+            tbProcesso.Focus();
+        }
+    }
+}
